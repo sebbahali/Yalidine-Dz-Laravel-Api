484773bbcf4323ec11ba3633c2692897b3de0ef7
sebbah nour <125546478+sebbahali@users.noreply.github.com>
2023-05-27 07:35:47 -0700

    Update YaledineServiceProvider.php

--- src/Providers/YaledineServiceProvider.php
+++ src/Providers/YaledineServiceProvider.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Config;
 
 use Illuminate\Support\ServiceProvider;
 
-class YaleServiceProvider extends ServiceProvider
+class YaledineServiceProvider extends ServiceProvider
 {
     public function boot()
     {
